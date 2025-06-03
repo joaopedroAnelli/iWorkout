d12e6342c60135be01c795bcdae2d6b1669f74b6
João Anelli <joaopedroanelli99@gmail.com>
2025-06-03 17:15:05 -0300

    Initial Commit

---
+++ iWorkout Watch App/iWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWorkoutApp.swift
+//  iWorkout Watch App
+//
+//  Created by João Anelli on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct iWorkout_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iWorkout/iWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWorkoutApp.swift
+//  iWorkout
+//
+//  Created by João Anelli on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct iWorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
