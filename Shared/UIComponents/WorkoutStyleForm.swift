7b8cd7bcb6824d67a2e273845d7907dc809e91cb
João Anelli <joaopedroanelli99@gmail.com>
2025-06-20 20:29:10 -0300

    Add a hint to progression field

--- Shared/UIComponents/WorkoutStyleForm.swift
+++ Shared/UIComponents/WorkoutStyleForm.swift
@@ -9,13 +9,19 @@ struct WorkoutStyleForm: View {
     var body: some View {
         Form {
             TextField("Style name", text: $name)
-            Picker("Session Progression", selection: $transition) {
-                ForEach(DivisionTransition.allCases) { Text($0.localized).tag($0) }
-            }
-            .help(Text("Session Progression Help"))
             Toggle("Active", isOn: $isActive)
             if isActive {
                 DatePicker("Active Until", selection: $activeUntil, in: Date()..., displayedComponents: [.date, .hourAndMinute])
+            }
+            Section(header: Text("About Sessions")) {
+                Picker("Session Progression", selection: $transition) {
+                    ForEach(DivisionTransition.allCases) { Text($0.localized).tag($0) }
+                }
+                .help(Text("Session Progression Help"))
+
+                Text("Session Progression Help")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
             }
         }
         .animation(.default, value: isActive)
